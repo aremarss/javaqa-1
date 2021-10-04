90d90fd04b984a41e8000468a7dda72ed9d23fd3
aremarss <aremars@mail.ru>
2021-10-04 19:39:31 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 2_000_000_000;
+        int sumTransfer = 500_000_000;
+        int total = currentBalance + sumTransfer;
+        System.out.println(total);
+    }
+}
